53b9c53145d819ec7a31245c09fa287e54230327
Aiden Cohen <74383229+DaGoat42@users.noreply.github.com>
2024-11-13 07:19:27 -0500

    Update main.js to be better

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let rectx = 940;
 let recty = 362;
-let width = 1880;
-let height = 725;
+let width;
+let height;
 let speedy = 1;
 let speedx = 1;
 let mincolor;
@@ -13,77 +13,88 @@ let bstep = 1;
 let gstep = 1;
 let sw = 50;
 let START = false;
+
 function sliderChangeSW(val) {
-sw = parseInt(val)
-}
-function sliderChangeSY(valy) {
-speedy = parseFloat(valy)
-}
-function sliderChangeSX(valx) {
-speedx = parseFloat(valx)
-}
-function sliderChangeR(valr) {
-r = parseInt(valr);
-}
-function sliderChangeG(valg) {
-g = parseInt(valg);
-}
-function sliderChangeB(valb) {
-b = parseInt(valb);
+    sw = parseInt(val);
 }
 
-function buttonStart(){
-START = true;
+function sliderChangeSY(valy) {
+    speedy = parseFloat(valy);
 }
-function buttonStop(){
-START = false;
+
+function sliderChangeSX(valx) {
+    speedx = parseFloat(valx);
 }
-console.log("i got here")
-function setup(){
-mincolor = random(70,155);
-        width = windowWidth - 40;
-createCanvas(width,height);
-frameRate(1000);
-background(0);
-r = random(70,256);
-g = random(70,256);
-b = random(70,256);
+
+function sliderChangeR(valr) {
+    r = parseInt(valr);
 }
-function draw(){
-if(START){
-//rect(x, y, width, height)
-fill(r,g,b);
-strokeWeight(0);
-rect(rectx, recty, sw, sw);
-if (rectx>=width-sw||rectx<=0){
-speedx = -speedx
+
+function sliderChangeG(valg) {
+    g = parseInt(valg);
 }
-if (recty>=height-sw||recty<=0){
-speedy = -speedy
+
+function sliderChangeB(valb) {
+    b = parseInt(valb);
 }
-rectx +=speedx;
-recty +=speedy;
-if (r>=255){
-rstep = -1;
+
+function buttonStart() {
+    START = true;
 }
-if (g>=255){
-gstep = -1;
+
+function buttonStop() {
+    START = false;
 }
-if (b>=255){
-bstep = -1;
+
+console.log("i got here");
+
+function setup() {
+    mincolor = random(70, 155);
+    width = windowWidth;
+    height = windowHeight;
+    createCanvas(width, height);
+    frameRate(1000);
+    background(0);
+    r = random(70, 256);
+    g = random(70, 256);
+    b = random(70, 256);
 }
-if (r<=mincolor){
-rstep = 1;
-}
-if (g<=mincolor){
-gstep = 1;
-}
-if (b<=mincolor){
-bstep = 1;
-}
-r = (r+rstep);
-g = (g+gstep);
-b = (b+bstep);
-console.log(r,g,b)
-}
+
+function draw() {
+    if (START) {
+        //rect(x, y, width, height)
+        fill(r, g, b);
+        strokeWeight(0);
+        rect(rectx, recty, sw, sw);
+        if (rectx >= width - sw || rectx <= 0) {
+            speedx = -speedx;
+        }
+        if (recty >= height - sw || recty <= 0) {
+            speedy = -speedy;
+        }
+        rectx += speedx;
+        recty += speedy;
+        if (r >= 255) {
+            rstep = -1;
+        }
+        if (g >= 255) {
+            gstep = -1;
+        }
+        if (b >= 255) {
+            bstep = -1;
+        }
+        if (r <= mincolor) {
+            rstep = 1;
+        }
+        if (g <= mincolor) {
+            gstep = 1;
+        }
+        if (b <= mincolor) {
+            bstep = 1;
+        }
+        r = (r + rstep);
+        g = (g + gstep);
+        b = (b + bstep);
+        console.log(r, g, b);
+    }
 }
